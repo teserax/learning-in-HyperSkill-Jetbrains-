9ae41591034d0b3a1cc7b7f00628d39ec37952c2
TESERAC <steeles80@gmail.com>
2024-01-12 18:06:37 +0200

    task 1

---
+++ untitled/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+      String x="O X X\n" +
+               "O X O\n" +
+               "X O X\n";
+        Scanner scanner=new Scanner(System.in);
+        char []nus=x.toCharArray();
+        System.out.println(nus[8]);
+        for(int i=0;i< nus.length;i++){
+            if (nus[0]=='O') {
+
+            }
+        }
+
+    }
+}
